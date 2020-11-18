926afae08e32ae2375122b28873ae09bb8140c19
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2020-11-18 16:37:05 -0300

    Final project

---
+++ App.js
@@ -0,0 +1,101 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Image,
+  Text,
+  TouchableOpacity
+} from 'react-native';
+
+const frases = [
+  'Siga os bons e aprenda com eles.',
+  'O bom-senso vale mais do que muito conhecimento.',
+  'O riso é a menor distância entre duas pessoas.',
+  'Deixe de lado as preocupações e seja feliz.',
+  'Realize o óbvio, pense no improvável e conquiste o impossível.',
+  'Acredite em milagres, mas não dependa deles.',
+  'A maior barreira para o sucesso é o medo do fracasso.'
+]
+
+const App = () => {
+
+  const [textoFrase, setTextoFrase] = React.useState('')
+  const [img, setImg] = React.useState(require('./src/biscoito-fechado.png'))
+
+
+
+  const quebrarBiscoito = () => {
+    const numeroAleatorio = Math.floor(Math.random() * frases.length)
+    setImg(require('./src/biscoito-aberto.png'))
+    setTextoFrase(frases[numeroAleatorio])
+  }
+
+  return (
+    <>
+      <View style={styles.container}>
+
+
+        <Image
+          source={img}
+          style={styles.img}
+        />
+
+        <Text style={styles.textoFrase}>{`" ${textoFrase} "`}</Text>
+
+        <TouchableOpacity style={styles.botao} onPress={quebrarBiscoito}>
+          <View style={styles.btnArea}>
+            <Text style={styles.btnTexto}>Quebrar Biscoito</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  img: {
+    width: 250,
+    height: 250
+  },
+  textoFrase: {
+    fontSize: 20,
+    color: '#dd7b22',
+    margin: 30,
+    fontStyle: 'italic',
+    textAlign: 'center'
+  },
+  botao: {
+    width: 230,
+    height: 50,
+    borderWidth: 2,
+    borderColor: '#dd7b22',
+    borderRadius: 25
+  },
+  btnArea: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  btnTexto: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#dd7b22',
+  }
+});
+
+export default App;
